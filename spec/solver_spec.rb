5e58cef5214215604bba09359caf6b23266a560d
jr-cast <castanos@pm.me>
2022-08-04 14:16:48 -0500

    Add test for solver fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,11 +8,18 @@ describe Solver do
       expect(solver.factorial(5)).to eq(120)
     end
 
-    it 'Reverses a string with the reverse method' do
+    it 'reverses a string with the reverse method' do
       solver = Solver.new
       expect(solver.reverse('hello')).to eq('olleh')
       expect(solver.reverse('microverse')).to eq('esrevorcim')
     end
 
+    it 'fizzbuzz behaves according to input' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+      expect(solver.fizzbuzz(35)).to eq('buzz')
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
   end
 end
